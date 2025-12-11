5315fecc5626a164744ba63e03a7e5c7791e926d
Matias Lombardi <129490797+Lombardimn@users.noreply.github.com>
2025-12-11 17:44:21 -0300

    Add Tailwind CSS configuration file

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'day1': '#bbf7d0',
+        'day2': '#16a34a',
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
